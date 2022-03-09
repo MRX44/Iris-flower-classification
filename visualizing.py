979e8f26ae8c6014edd7eb216212fc778272fc67
MRX44 <mohamedelfeky202020@gmail.com>
2022-03-09 11:50:43 +0200

    training and predicting

--- visualizing.py
+++ visualizing.py
@@ -3,10 +3,10 @@ import pandas as pd
 import scipy as sp
 import matplotlib.pyplot as plt
 from mlxtend.plotting import scatterplotmatrix
-from mlxtend.plotting import scatterplotmatrix
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from mlxtend.plotting import plot_decision_regions
 
 iris = load_iris()
 X, y = iris.data[:, 2:], iris.target
@@ -15,8 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     random_state=123,
                                                     shuffle=True)
 
-
-
+#plotting all featues
 names = ['sepal length [cm]', 'sepal width [cm]',
          'petal length [cm]', 'petal width [cm]']
 
@@ -24,4 +23,26 @@ fig, axes = scatterplotmatrix(iris.data[y==0], figsize=(10, 8), alpha=0.5)
 fig, axes = scatterplotmatrix(iris.data[y==1], fig_axes=(fig, axes), alpha=0.5)
 fig, axes = scatterplotmatrix(iris.data[y==2], fig_axes=(fig, axes), alpha=0.5, names=names)
 plt.tight_layout()
+#plotting the selected features
+selected_names =['petal length [cm]', 'petal width [cm]']
+fig2,axes2 = scatterplotmatrix(X_train[y_train==0],figsize=(10,8),alpha=0.5)
+fig2,axes2 = scatterplotmatrix(X_train[y_train==1],fig_axes=(fig2,axes2),alpha=0.5)
+fig2,axes2 = scatterplotmatrix(X_train[y_train==2],fig_axes=(fig2,axes2),alpha=0.5,names=selected_names)
+
+#fitting the model
+knn_model = KNeighborsClassifier(n_neighbors=3)
+knn_model.fit(X_train,y_train)
+
+#making predictions
+y_pred = knn_model.predict(X_test)
+value= y_test == y_pred
+score = knn_model.score(X_test,y_test)
+print('Test set accuarcy: {:.2f}%'.format(score*100))
+
+#plotting Decision boundary
+fig3 =  plt.figure() 
+plot_decision_regions(X_train,y_train,knn_model)
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
 plt.show()
